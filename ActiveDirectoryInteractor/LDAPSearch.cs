8a0947529421db079e18468bbfd9360c28a4374d
Evan Stevenson <evanstevenson13@gmail.com>
2016-04-24 17:28:23 -0400

    Created project and test

---
+++ ActiveDirectoryInteractor/LDAPSearch.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.DirectoryServices;
+
+
+namespace ActiveDirectoryInteractor{
+    public static class LDAPSearch{
+        private static string path = string.Empty;
+        private static string username = string.Empty;
+        private static string password = string.Empty;
+        private static Dictionary<string, object> allProperties = new Dictionary<string, object>();
+        public static string fullPath{
+            get{
+                if(string.IsNullOrEmpty(path)){return string.Empty;}
+                string domain = string.Join(".", path.Split('.'));
+                string completePath = string.Empty;
+                completePath += "LDAP://";
+                completePath += domain + "/DC=";
+                completePath += string.Join(",DC=", path.Split('.'));
+                return completePath;
+            }
+            set{}
+        }
+
+
+        public static void Initialize(string path, string username, string password){
+            LDAPSearch.path = path;
+            LDAPSearch.username =  username;
+            LDAPSearch.password = password;
+        }
+
+
+        public static Dictionary<string, object> GetComputerProperties(string computerName){
+            //if(!DirectoryEntry.Exists(fullPath)){}
+            DirectoryEntry de = new DirectoryEntry(fullPath, username, password, AuthenticationTypes.Secure);
+            DirectorySearcher deSearch = new DirectorySearcher(de);
+
+            deSearch.SearchScope = SearchScope.Subtree;
+            deSearch.Filter = "(&(objectClass=computer)(name=" + computerName + "))";
+            //deSearch.PropertiesToLoad.Add("ms-Mcs-AdmPwd");
+            deSearch.ClientTimeout = new TimeSpan(0, 0, 5);
+            SearchResult results = deSearch.FindOne();
+
+            ResultPropertyCollection properties = results.Properties;
+
+            allProperties.Clear();
+            foreach(DictionaryEntry property in results.Properties){
+                allProperties.Add(property.Key.ToString(), property.Value);
+            }
+
+            de.Dispose();
+            return allProperties;
+        }
+
+    }// Class
+
+}// Namespace
---
+++ ActiveDirectoryInteractorTest/LDAPSearchTest.cs
@@ -0,0 +1,20 @@
+﻿
+
+using ActiveDirectoryInteractor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ActiveDirectoryInteractorTest {
+    [TestClass]
+    public class LDAPSearchTest{
+        
+        /// <summary>
+        /// Checks that the correct ldap uri is generated
+        /// </summary>
+        [TestMethod]
+        public void CorrectLDAPURI(){
+            LDAPSearch.Initialize("sub1.sub2.sub3.sub4.main", "", "");
+            string expected = "LDAP://sub1.sub2.sub3.sub4.main/DC=sub1,DC=sub2,DC=sub3,DC=sub4,DC=main";
+            Assert.AreEqual(expected, LDAPSearch.fullPath);
+        }
+    }
+}
