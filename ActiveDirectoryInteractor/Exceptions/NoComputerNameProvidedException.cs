9f2a29789668fe7e9186c910949516257c7f0217
evanstevenson13 <evanstevenson13@gmail.com>
2016-04-28 09:54:18 -0400

    Added exceptions folder

---
+++ ActiveDirectoryInteractor/Exceptions/NoComputerNameProvidedException.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+
+
+namespace Device42Interactor{
+    public class NoComputerNameProvidedException : Exception{
+        public NoComputerNameProvidedException(){
+                
+        }
+
+        public NoComputerNameProvidedException(string message) : base(message){
+
+        }
+
+        public NoComputerNameProvidedException(string message, Exception inner) : base(message, inner){
+
+        }
+    }
+}
